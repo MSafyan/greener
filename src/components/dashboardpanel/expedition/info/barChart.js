fe383a8230b7c3def85b16de0131aa8be44abdb3
MSafyan <msafyan080@gmail.com>
2022-09-11 19:04:52 +0500

    arrow fade

--- src/components/dashboardpanel/expedition/info/barChart.js
+++ src/components/dashboardpanel/expedition/info/barChart.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import SelectionDropdown from './selectionDropdown';
 import { connect } from 'react-redux';
-import { Link, animateScroll as scroll } from 'react-scroll';
+// import { Link, animateScroll as scroll } from 'react-scroll';
 
 const BarChart = ({ dashboardData }) => {
 	const barRef = React.useRef(null);
 	const [showLeft, setShowLeft] = React.useState(false);
 	const [showRight, setShowRight] = React.useState(false);
 
-	const onScroll = (e) => {
-		setShowLeft(e.target.scrollLeft > 0);
-		setShowRight(
-			!(e.target.scrollWidth <= e.target.scrollLeft + e.target.clientWidth)
-		);
-	};
+	React.useEffect(() => {
+		const onScroll = (e) => {
+			setShowLeft(e.target.scrollLeft);
+
+			setShowRight(
+				!(
+					Math.ceil(e.target.scrollWidth) <=
+					Math.ceil(e.target.scrollLeft + e.target.clientWidth)
+				)
+			);
+		};
+		barRef.current.addEventListener('scroll', onScroll);
+
+		return () => barRef.current.removeEventListener('scroll', onScroll);
+	}, [showLeft]);
 
 	const Arrow = (icon) => {
 		var right = icon.includes('right');
 
 		const show = () => {
-			if ((right && showRight) || (!right && showLeft)) {
+			if ((right && showRight) || (!right && showLeft > 0)) {
 				return 'block';
 			}
 			return 'none';
@@ -27,9 +36,10 @@ const BarChart = ({ dashboardData }) => {
 		return (
 			<div
 				style={{
-					paddingBottom: '15%',
 					display: show(),
+					right: right ? '1px' : '',
 				}}
+				className='arrow_info'
 				onClick={() => {
 					if (right) {
 						barRef.current.scrollLeft += barRef.current.clientWidth;
@@ -38,7 +48,9 @@ const BarChart = ({ dashboardData }) => {
 					}
 				}}
 			>
-				<i className={`fas ${icon} fs-16 s-icon`}></i>
+				<div className={right ? 'chevron-right' : 'chevron-left'}>
+					<i className={`fas ${icon} fs-16 s-icon`}></i>
+				</div>
 			</div>
 		);
 	};
@@ -47,21 +59,30 @@ const BarChart = ({ dashboardData }) => {
 		<div className='barChart_div'>
 			<span className='vertical_text'>Expéditions crées</span>
 			<SelectionDropdown />
-			{Arrow('fa-arrow-left')}
-			<div className='bars_wrapper' ref={barRef} onScroll={onScroll}>
-				{dashboardData?.data?.data?.columns[0]?.map((_, i) => (
-					<div className='bar_wrapper' key={i}>
-						<div
-							className='bar bg-main'
-							style={{
-								height: `calc(20vh * calc(${_} / ${dashboardData.max})`,
-							}}
-						></div>
-						<p className='lc fs-20'>{_}</p>
-					</div>
-				))}
+			<div className='arrow_bars_wrapper'>
+				{Arrow('fa-chevron-left')}
+				<div className='bars_wrapper' ref={barRef}>
+					{dashboardData?.data?.data?.columns[0]?.map((_, i) => (
+						<div className='bar_wrapper' key={i}>
+							<div
+								className='bar bg-main'
+								style={{
+									height: `calc(20vh * calc(${_} / ${dashboardData.max})`,
+								}}
+							></div>
+							<p className='lc fs-20'>{_}</p>
+						</div>
+					))}
+				</div>
+				{Arrow('fa-chevron-right')}
 			</div>
-			{Arrow('fa-arrow-right')}
+			{/* {
+				<p style={{}}>
+					{barRef?.current?.scrollWidth +
+						'=' +
+						(barRef?.current?.scrollLeft + barRef?.current?.clientWidth)}
+				</p>
+			} */}
 		</div>
 	);
 };
