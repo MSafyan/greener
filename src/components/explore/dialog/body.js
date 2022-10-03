447bee54cddbc9cf2f0d24bb16356f2650863947
MSafyan <msafyan080@gmail.com>
2022-10-03 14:51:38 +0500

    card updated

--- src/components/explore/dialog/body.js
+++ src/components/explore/dialog/body.js
@@ -6,7 +6,128 @@ import NotificationRing from './popups/NoficationRing';
 import Inserer from './popups/inserer';
 import { connect } from 'react-redux';
 import MsgForm from './popups/MsgForm';
+import StarRatings from 'react-star-ratings';
+import audioDots from '../../../assets/images/audioDots.png';
+import audioPause from '../../../assets/images/audioPause.png';
+import trajet from '../../../assets/images/trajet.png';
+import activity from '../../../assets/images/activity.png';
+import incidents from '../../../assets/images/incidents.png';
+import document from '../../../assets/images/document.png';
+import msg from '../../../assets/images/msg.png';
+import offer from '../../../assets/images/offer.png';
+import clock from '../../../assets/images/clock.png';
 
+const imgNameArr = {
+	trajet: clock,
+	trajet: trajet,
+	activity: activity,
+	incidents: incidents,
+	document: document,
+	msg_text: msg,
+	msg_audio: msg,
+	offer: offer,
+};
+
+const Card = ({ _, i }) => {
+	var upperText = _.message;
+	var bottomText = _.creator;
+	var showBottomText = true;
+	var sideLabel = '';
+	if (_.componentType === 'trajet') {
+		showBottomText = false;
+		sideLabel = <NotificationRing />;
+	}
+	if (_.componentType === 'document') {
+		upperText = 'Document';
+		bottomText = _.message;
+		sideLabel = <p className='fs-16 tc'>Telecharger</p>;
+	}
+	if (_.componentType === 'incidents') {
+		sideLabel = <p className='fs-16 tc'>incidents</p>;
+	}
+
+	if (_.componentType === 'offer') {
+		return <OfferCard _={_} i={i} />;
+	}
+
+	return (
+		<div key={i} className='card_body'>
+			<div>
+				{_.componentType === 'msg_audio' ? (
+					<>
+						<div>
+							<img src={audioPause} alt='' />
+							<img src={audioDots} alt='' />
+						</div>
+					</>
+				) : (
+					<p className='fs-20'>{upperText}</p>
+				)}
+
+				{showBottomText && <p className='fs-16 lc'>{bottomText}</p>}
+			</div>
+			{sideLabel}
+		</div>
+	);
+};
+
+const cardWrapper = (_, i) => {
+	const iconRen = () => {
+		// var icon = icon;
+		// var index = Object.keys(imgNameArr).findIndex((val) => {
+		// 	debugger;
+		// 	console.log(_.componentType, val, _.componentType === val);
+		// 	return val === _.componentType;
+		// });
+		debugger;
+		var a = imgNameArr[_.componentType];
+		debugger;
+
+		return a;
+	};
+	return (
+		<div key={i} className='body_chargement_explore'>
+			<p className={i === 3 ? `fs-16` : 'fs-13 lc'}>
+				{moment(_.dateTime).format('D/MM/YYYY HH:mm')}
+			</p>
+			<div className='card_icons_explore'>
+				<div className='icon-wrapper-20 py-1'>
+					<img src={imgNameArr[_.componentType]} alt='' />
+				</div>
+				<div style={{ height: '2.2rem' }}>
+					<img src={dots} alt='' />
+				</div>
+			</div>
+			<div className='body_card_explore'>
+				<Card _={_} i={i} />
+			</div>
+		</div>
+	);
+};
+
+const OfferCard = ({ _, i }) => {
+	return (
+		<div key={i} className='card_body'>
+			<div>
+				<p className='fs-16 lc'>
+					{_.message}
+					<span className='dc font-weight-bold'>{' ' + _.price} $</span>
+				</p>
+				<div className='d-flex align-items-end'>
+					<StarRatings
+						rating={_.rating}
+						starRatedColor='#F59B21'
+						numberOfStars={5}
+						starDimension='18px'
+						starSpacing='1px'
+						name='rating'
+					/>
+					<p className='fs-16 lc pt-1'>{'   ' + _.ratesCount}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 const Body = ({ orderEvents, showInserersPop }) => {
 	return (
 		<div className='body_explore bg-card'>
@@ -16,32 +137,7 @@ const Body = ({ orderEvents, showInserersPop }) => {
 				{showInserersPop !== null ? (
 					<MsgForm showInserersPop={showInserersPop} />
 				) : (
-					orderEvents?.map((_, i) => {
-						return (
-							<div key={i} className='body_chargement_explore'>
-								<p className={i === 3 ? `fs-16` : 'fs-13 lc'}>
-									{moment(_.dateTime).format('D/MM/YYYY HH:mm')}
-								</p>
-								<div className='card_icons_explore'>
-									<div className='icon-wrapper-20 py-1'>
-										<img src={icon} alt='' />
-									</div>
-									<div style={{ height: '2.2rem' }}>
-										<img src={dots} alt='' />
-									</div>
-								</div>
-								<div className='body_card_explore'>
-									<div className=''>
-										<div>
-											<p className='fs-20'>{_.message.substring(0, 39)}</p>
-											<p className='fs-16 lc'>{_.creator}</p>
-										</div>
-										<NotificationRing />
-									</div>
-								</div>
-							</div>
-						);
-					})
+					orderEvents?.map((_, i) => cardWrapper(_, i))
 				)}
 			</div>
 			{/* </div> */}
--- src/components/explore/dialog/popups/MsgForm.js
+++ src/components/explore/dialog/popups/MsgForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import moment from 'moment';
-import Form from 'react-bootstrap/Form';
+// import Form from 'react-bootstrap/Form';
 import { connect } from 'react-redux';
-
+import { Form } from 'react-bootstrap';
 import { showInserersPopAction } from '../../../../store/actions/exploreAction';
 
 import icon from '../../../../assets/images/msg.png';
@@ -12,7 +12,91 @@ import cross from '../../../../assets/images/cross.png';
 
 import './popups.css';
 
-const MsgForm = ({ showInserersPopAction }) => {
+var options = { One: 1, Two: 2 };
+
+const Select = ({ options, lagend, size = 'lg' }) => {
+	return (
+		<Form.Select
+			className='fs-13'
+			bsPrefix='myselect'
+			aria-label='Default select example'
+			size={size}
+		>
+			{lagend && <option className='fs-16'>{lagend}</option>}
+
+			{Object.keys(options).map((_, i) => (
+				<option key={i} value={options[_]}>
+					{_}
+				</option>
+			))}
+		</Form.Select>
+	);
+};
+
+const Doc3 = () => {
+	return (
+		<div className='d-flex flex-column doc3_wrapper'>
+			<div className='d-flex justify-content-between align-items-center'>
+				<Select options={options} lagend='Open this select menu' size='sm' />
+				<Select options={options} lagend='Open this select menu' size='sm' />
+			</div>
+			<Form.Control
+				as='textarea'
+				placeholder='Votre message ...'
+				style={{
+					height: '85%',
+					padding: '4px',
+					fontSize: '16px',
+				}}
+			/>
+		</div>
+	);
+};
+const Doc2 = () => {
+	return (
+		<div className='d-flex justify-content-between align-items-end'>
+			<div>
+				<p className='fs-16 lc'>Document.exe</p>
+				<Select options={options} lagend='Open this select menu' />
+			</div>
+			<div>
+				<p className='tc'>Valider</p>
+			</div>
+		</div>
+	);
+};
+
+const Doc = () => {
+	return (
+		<div className='d-flex justify-content-start'>
+			<div>
+				<Form.Control
+					as='textarea'
+					placeholder='Votre message ...'
+					style={{
+						height: '85%',
+						padding: '4px',
+						fontSize: '16px',
+					}}
+				/>
+			</div>
+			<div className='msgForm_icons'>
+				<div className='icon-wrapper-15'>
+					<img src={paperclip} alt='' height='100%' />
+				</div>
+				<div className='icon-wrapper-15'>
+					<img src={mic} alt='' height='100%' />
+				</div>
+			</div>
+		</div>
+	);
+};
+const MsgForm = ({ showInserersPopAction, showInserersPop }) => {
+	const DocRen = () => {
+		if (showInserersPop === 0) return <Doc />;
+		if (showInserersPop === 1) return <Doc2 />;
+		if (showInserersPop === 2) return <Doc3 />;
+	};
 	return (
 		<div className='body_chargement_explore'>
 			<p className='fs-16 px-2'>{moment().format('D/MM/YYYY HH:mm')}</p>
@@ -22,34 +106,14 @@ const MsgForm = ({ showInserersPopAction }) => {
 				</div>
 			</div>
 			<div className='body_card_explore'>
-				<div className='d-flex justify-content-start'>
-					<div>
-						<Form.Control
-							as='textarea'
-							placeholder='Votre message ...'
-							style={{
-								height: '85%',
-								padding: '4px',
-								fontSize: '16px',
-							}}
-						/>
-					</div>
-					<div className='msgForm_icons'>
-						<div className='icon-wrapper-15'>
-							<img src={paperclip} alt='' height='100%' />
-						</div>
-						<div className='icon-wrapper-15'>
-							<img src={mic} alt='' height='100%' />
-						</div>
-					</div>
-					<div
-						className='icon-wrapper-15 cross_popup'
-						onClick={() => {
-							showInserersPopAction(null);
-						}}
-					>
-						<img src={cross} alt='' height='140%' />
-					</div>
+				{DocRen()}
+				<div
+					className='icon-wrapper-15 cross_popup'
+					onClick={() => {
+						showInserersPopAction(null);
+					}}
+				>
+					<img src={cross} alt='' height='140%' />
 				</div>
 			</div>
 		</div>
